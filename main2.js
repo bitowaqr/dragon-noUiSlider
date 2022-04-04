badc52a66429692a34fe4cd8ee19a043f6732783
bitowaqr <bitowaqr@gmail.com>
2022-04-04 09:23:12 +0200

    bug fix

--- main2.js
+++ main2.js
@@ -11,52 +11,122 @@ options = {
         'max': 100
     }
 }
-
+var connects;
 createSlider = function (slider, options) {
     noUiSlider.create(slider, options);
     const origins = slider.getElementsByClassName('noUi-origin');
     origins[0].setAttribute('disabled', true);
     origins[1].setAttribute('disabled', true);
+    connects = document.querySelectorAll('.noUi-connect');
 }
 createSlider(slider, options);
 
+
 var draggedHandle;
-var enableDragDrop = true;
 var draggedElement;
 const draggableHandles = document.querySelectorAll('.drag-handle');
+const dropzone = document.querySelector(".dropzone");
 
+const highlightCol = "cyan";
+const defaultCol = "lightgray";
+lightSlider = function (slider, col) {
+    connects = document.querySelectorAll('.noUi-connect');
+    connects.forEach(element => {
+        element.style.backgroundColor = col;
+    })
+};
+
+touchMoveListener = function (e) {
+    console.log("touchMoveListener");
+    e.preventDefault();
+    var evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent;
+    var touch = evt.touches[0] || evt.changedTouches[0];
+    const touchTargetUnderneath = document.elementFromPoint(touch.pageX - window.pageXOffset, touch.pageY - window.pageYOffset);
+    this.style.top = touch.pageY - this.offsetHeight / 2 + 'px';
+    this.style.left = touch.pageX - this.offsetWidth + 'px';
+
+    console.log(touchTargetUnderneath)
+    if (!touchTargetUnderneath) {
+        connects.forEach(el => {
+            if (el.classList.contains('connect-active')) {
+                el.classList.remove('connect-active');
+            }
+        })
+        return;
+    }
+
+    if (
+        touchTargetUnderneath.classList.contains('noUi-base') ||
+        touchTargetUnderneath.classList.contains('dropzone')
+    ) {
+
+        connects.forEach(el => {
+            if (!el.classList.contains('connect-active')) {
+                el.classList.add('connect-active');
+            }
+        })
+    } else {
+        connects.forEach(el => {
+            if (el.classList.contains('connect-active')) {
+                el.classList.remove('connect-active');
+            }
+        })
+    }
+}
 
 draggableHandles.forEach(handle => {
 
     handle.addEventListener('dragstart', (evt) => {
-        evt.dataTransfer.setData('text/plain',null);
+        console.log("dragstart");
+        evt.dataTransfer.setData('text/plain', null);
         draggedElement = evt.target;
         handle.classList.add('active-dragging');
         evt.target.style.opacity = 0.5;
     })
 
     handle.addEventListener('dragend', (evt) => {
-        handle.classList.remove('active-dragging');
-        dragState = false;
+        console.log("dragend");
         evt.target.style.opacity = 1;
         draggedElement = null;
+
     })
-    
+
     // touch workaround
     handle.addEventListener('touchstart', (evt) => {
+        console.log("touchstart");
+        handle.style.position = 'absolute';
         draggedElement = handle;
     })
 
-    handle.addEventListener('touchmove', function (e) {
-        handle.classList.add('active-dragging');
+    handle.addEventListener('touchmove', touchMoveListener)
+
+    handle.addEventListener('touchend', function (e) {
+        console.log("touchend");
         var evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent;
         var touch = evt.touches[0] || evt.changedTouches[0];
-        touchTargetUnderneath = document.elementFromPoint(touch.pageX, touch.pageY);    
-        handle.style.position = 'absolute';
-        handle.style.left = touch.pageX - handle.offsetWidth + 'px';
-        handle.style.top = touch.pageY - handle.offsetHeight/2 + 'px';
-        
-        if (touchTargetUnderneath.classList.contains('noUi-base')) {
+        const touchTargetUnderneath = document.elementFromPoint(touch.pageX - window.pageXOffset, touch.pageY - window.pageYOffset);
+
+
+        if (dropzone.classList.contains('dropzone-active')) {
+            dropzone.classList.remove('dropzone-active');
+        }
+
+        if (!touchTargetUnderneath) {
+            connects.forEach(el => {
+                if (el.classList.contains('connect-active')) {
+                    el.classList.remove('connect-active');
+                }
+            })
+            console.log("reset")
+            handle.style = '';
+            handle.style.position = 'relative';
+            return;
+        }
+
+        if (
+            touchTargetUnderneath.classList.contains('noUi-base') ||
+            touchTargetUnderneath.classList.contains('dropzone')
+        ) {
             console.log(draggedElement.dataset)
             var newVal = slider.noUiSlider.calcPointToPercentage(touch.pageY)
             newVal = newVal >= 100 ? 99 : newVal;
@@ -69,29 +139,65 @@ draggableHandles.forEach(handle => {
             slider.noUiSlider.destroy();
             options.start = values;
             createSlider(slider, options);
-
+            this.removeEventListener('touchmove', touchMoveListener);
             draggedElement = null;
+        } else {
+            console.log("reset")
+            handle.style = '';
+            handle.style.position = 'relative';
         }
-        
+
+
+
+
+
+
     })
-    handle.addEventListener('touchend', function (e) {
-        handle.style = '';
-        handle.style.position = 'relative';
-        handle.classList.remove('active-dragging');
-        draggedElement = null;
+
+    handle.addEventListener('touchcancel', function (e) {
+        console.log("touchcancel");
+        var evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent;
+        var touch = evt.touches[0] || evt.changedTouches[0];
+        touchTargetUnderneath = document.elementFromPoint(touch.pageX, touch.pageY);
+
+        if (dropzone.classList.contains('dropzone-active')) {
+            dropzone.classList.remove('dropzone-active');
+        }
+
+        if (
+            touchTargetUnderneath.classList.contains('noUi-base') ||
+            touchTargetUnderneath.classList.contains('dropzone')
+        ) {
+            console.log(draggedElement.dataset)
+            var newVal = slider.noUiSlider.calcPointToPercentage(touch.pageY)
+            newVal = newVal >= 100 ? 99 : newVal;
+            newVal = newVal <= 0 ? 1 : newVal;
+
+            draggedElement.remove()
+
+            var values = slider.noUiSlider.get(true);
+            values.push(newVal); // Your new handle start value
+            slider.noUiSlider.destroy();
+            options.start = values;
+            createSlider(slider, options);
+            this.removeEventListener('touchmove', touchMoveListener);
+            draggedElement = null;
+        } else {
+            handle.style = '';
+            handle.style.position = 'relative';
+        }
+
+
+
+
+
+
     })
-    
+
 })
 
-const dropzone = document.querySelector(".dropzone");
-const highlightCol = "cyan";
-const defaultCol = "lightgray";
-lightSlider = function (slider, col) {
-    connects = document.querySelectorAll('.noUi-connect');
-    connects.forEach(element => {
-        element.style.backgroundColor = col;
-    })
-};
+
+
 dropzone.addEventListener("dragenter", function (evt) {
     lightSlider(slider, highlightCol);
     evt.preventDefault();
@@ -110,45 +216,21 @@ dropzone.addEventListener("dragleave", function (evt) {
 dropzone.addEventListener("drop", function (evt) {
     console.log("drop")
     evt.preventDefault();
-    
+
     console.log(draggedElement)
-    var newVal = slider.noUiSlider.calcPointToPercentage(evt.y)
+    var newVal = slider.noUiSlider.calcPointToPercentage(evt.y + window.pageYOffset)
     newVal = newVal >= 100 ? 99 : newVal;
     newVal = newVal <= 0 ? 1 : newVal;
 
     draggedHandle = document.querySelector('.active-dragging')
     draggedHandle.remove()
-    
+
     var values = slider.noUiSlider.get(true);
     values.push(newVal); // Your new handle start value
     slider.noUiSlider.destroy();
     options.start = values;
     createSlider(slider, options);
-    
+
     draggedElement = null;
 })
 
-
-// dropzone.addEventListener("dragenter", function (evt) {
-//     console.log("target hit")
-//     // evt.preventDefault();
-    
-//     if (!dragState)
-//         return;
-    
-//     dragState = false;
-    
-//     var newVal = slider.noUiSlider.calcPointToPercentage(evt.y)
-//     newVal = newVal >= 100 ? 99 : newVal;
-//     newVal = newVal <= 0 ? 1 : newVal;
-
-//     draggedHandle = document.querySelector('.active-dragging')
-//     draggedHandle.remove()
-    
-//     var values = slider.noUiSlider.get(true);
-//     values.push(newVal); // Your new handle start value
-//     slider.noUiSlider.destroy();
-//     options.start = values;
-//     createSlider(slider, options);
-// })
-  
